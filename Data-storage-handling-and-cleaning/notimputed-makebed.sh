b17326b2323d924c53a16178c2faf8d8ec8d5c4a
Cristina Abbatangelo <116049572+clabba@users.noreply.github.com>
2022-11-01 15:26:19 -0400

    Update notimputed-makebed.sh

--- Data-storage-handling-and-cleaning/notimputed-makebed.sh
+++ Data-storage-handling-and-cleaning/notimputed-makebed.sh
@@ -13,4 +13,4 @@ module load StdEnv/2020 plink/1.9b_6.21-x86_64
 plink \
         --vcf  /path-to-vcf-file/filename.vcf.gz --make-bed --out outputprefix
 
-#Note: PLINK2 utilizes slightly different binary files termed pgen/pvar/psam, if bed/bim/fam files are preferred, using PLINK1.9 will do the trick
+#Note: PLINK2 utilizes slightly different binary files termed pgen/pvar/psam; if bed/bim/fam files are preferred, using PLINK1.9 will do the trick
