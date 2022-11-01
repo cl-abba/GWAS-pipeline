1bd4ede7ffd35c77fe66641242d2a30da0852059
Cristina Abbatangelo <116049572+clabba@users.noreply.github.com>
2022-11-01 15:25:54 -0400

    Update generate-sample-file.R

--- Data-storage-handling-and-cleaning/generate-sample-file.R
+++ Data-storage-handling-and-cleaning/generate-sample-file.R
@@ -1,7 +1,7 @@
 df <- read.delim("filename.txt", sep = " ", header = TRUE)
 head(df)
 
-df_filter <- df %>% filter(A1 == "2", A2 == "2") # This line is if specific genotypes are required, can remove lines pertaining to filtering if not required.
+df_filter <- df %>% filter(A1 == "2", A2 == "2") # This line is if specific genotypes are required, can remove lines pertaining to filtering if not required
 head(df_filter)
 
 df_filter_sub <- df_filter[,-c(3:8)]
