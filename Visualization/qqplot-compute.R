dc999d470c22074cfd319d24d04b1377492fadf8
Cristina Abbatangelo <116049572+clabba@users.noreply.github.com>
2022-11-02 09:35:04 -0400

    Create qqplot-compute.R

---
+++ Visualization/qqplot-compute.R
@@ -0,0 +1,5 @@
+# setwd()
+
+post_gwas_plots(path_to_folder = "/path-to-SAIGE-output-files", 
+                file_to_read1 = "output-pvalQQ.out", 
+                file_name = "outputsuffix", lambda_pos = 10)
